bf41c1d764403c5c6922f1d7a232dbea93f5dd7e
Hassan ZEROUAL <zerhassan@gmail.com>
2016-12-22 11:16:51 +0000

    module statistique de l'admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Ticket;
+use App\Priorite;
 use Auth;
 class HomeController extends Controller
 {
@@ -24,6 +25,15 @@ class HomeController extends Controller
      */
     public function index()
     {
+      $statistiques=[];
+      $statistiques['total']=Ticket::All()->count();
+      $statistiques['encours']=Ticket::where('etat','en cours')->get()->count();
+      $statistiques['creation']=Ticket::where('etat','création')->get()->count();
+      $statistiques['realisee']=Ticket::where('etat','réalisée')->get()->count();
+
+      $priorites=Priorite::All();
+
+
       if(Auth::user()->role=='admin')
       {
           $tickets=Ticket::paginate(10);
@@ -33,6 +43,6 @@ class HomeController extends Controller
           $tickets=Ticket::where('user_id',Auth::user()->id)->paginate(10);
       }
 
-      return view('home',compact('tickets'));
+      return view('home',compact('tickets','statistiques','priorites'));
     }
 }
--- resources/views/ticket/admin.blade.php
+++ resources/views/ticket/admin.blade.php
@@ -1,19 +1,73 @@
-<table class="table table-striped table-bordered table-hovered ">
-  <tr>
-    <td>Priorité</td>
-    <td>Etat</td>
-    <td>Utilisateur</td>
-    <td>Actions</td>
-  </tr>
-  @foreach ($tickets as $ticket)
+<div class="panel panel-primary">
+  <div class="panel-heading">
+    <h3 class="panel-title">Statistiques</h3>
+  </div>
+  <div class="panel-body">
+  <table class="table table-bordered">
     <tr>
-      <td>{{ $ticket->priorite->nom}}</td>
-      <td>{{ $ticket->etat}}</td>
-      <td>{{ $ticket->user->name}}</td>
+      <td>Total des tickets :<span class="badge">{{ $statistiques['total'] }}</span></td>
+      <td>Total des utilisateurs : <span class="badge">{{  \App\User::All()->count() }}</span></td>
+    </tr>
+    <tr>
       <td>
-        <a href="{{ url('ticket/'.$ticket->id.'/consulter')}}" class="btn btn-info">Consulter</a>
+        <table class="table table-bordered table-striped">
+          <tr>
+            <td colspan="2"> <b>Etats</b> </td>
+          </tr>
+          <tr>
+            <td>Création</td>
+            <td><span class="badge">{{ $statistiques['creation']}}</span></td>
+          </tr>
+          <tr>
+            <td>En cours</td>
+            <td><span class="badge">{{ $statistiques['encours']}}</span></td>
+          </tr>
+          <tr>
+            <td>Réalisée</td>
+            <td><span class="badge">{{ $statistiques['realisee']}}</span></td>
+          </tr>
+        </table>
+      </td>
+      <td>
+        <table class="table table-bordered table-striped">
+          <tr>
+            <td colspan="2"> <b>Priorités</b> </td>
+          </tr>
+          @foreach ($priorites as $priorite)
+           <tr>
+             <td>{{ $priorite->nom}}</td>
+             <td><span class="badge">{{ $priorite->tickets->count()}}</span></td>
+           </tr>
+          @endforeach
+        </table>
       </td>
     </tr>
-  @endforeach
-</table>
-{{ $tickets->render() }}
+  </table>
+  </div>
+</div>
+<div class="panel panel-success">
+  <div class="panel-heading">
+    <h3 class="panel-title">Liste des tickets</h3>
+  </div>
+  <div class="panel-body">
+    <table class="table table-striped table-bordered table-hovered ">
+      <tr>
+        <td>Priorité</td>
+        <td>Etat</td>
+        <td>Utilisateur</td>
+        <td>Actions</td>
+      </tr>
+      @foreach ($tickets as $ticket)
+        <tr>
+          <td>{{ $ticket->priorite->nom}}</td>
+          <td>{{ $ticket->etat}}</td>
+          <td>{{ $ticket->user->name}}</td>
+          <td>
+            <a href="{{ url('ticket/'.$ticket->id.'/consulter')}}" class="btn btn-info">Consulter</a>
+          </td>
+        </tr>
+      @endforeach
+    </table>
+    {{ $tickets->render() }}
+  </div>
+</div>
